d0e711264d2da1eb4d74362b43174cb57f6f6ce3
Usernameorsmth <simidarawilliams@gmial.com>
2025-05-31 18:58:58 +0100

    smh

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 try:
-    num = int(input("Enter your number: "))
-    print(num)
-except ValueError as ex:
-    print("Exception:", ex)
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter a number: "))
+    result = num1 / num2
+    print("Result is:", result)
+    print("Result is:", result)
 
-print("I am outside the try block")
+except ZeroDivisionError:
+    print("Division by zero is not allowed")
+
+except ValueError:
+    print("Please enter numerical value")
+
+except NameError as ex:
+    print("The exception is", ex)
+
+except:
+    print("Some error occurred")
+
+finally:
+    print("I will execute no matter what happens")
