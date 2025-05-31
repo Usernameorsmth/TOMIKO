4eda11ea0da2ce78acbb1680c0d166b67ab4c846
Usernameorsmth <simidarawilliams@gmial.com>
2025-05-31 19:13:20 +0100

    pan

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
-try:
-    num1 = int(input("Enter a number: "))
-    num2 = int(input("Enter a number: "))
-    result = num1 / num2
-    print("Result is:", result)
-    print("Result is:", result)
+valid = False
 
-except ZeroDivisionError:
-    print("Division by zero is not allowed")
-
-except ValueError:
-    print("Please enter numerical value")
-
-except NameError as ex:
-    print("The exception is", ex)
-
-except:
-    print("Some error occurred")
-
-finally:
-    print("I will execute no matter what happens")
+while not valid:
+    try:
+        n = int(input("Enter a number: "))  # enter an even number
+        while n % 2 == 0:
+            print("bye")
+            valid = True
+    except ValueError:
+        print("Invalid")
